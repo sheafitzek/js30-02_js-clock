2c2c6ddf4ae1c84418011560dd5dec3504ead5b2
Shea Fitzek <agg817@sbcglobal.net>
2017-08-20 00:58:37 -0500

    styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,41 @@
 const app = {
-	onloadFunction() {
-		const startApp = new Promise(()=> {
-			app.setDate();
-		});
-
-		startApp
-			.then(setInterval(app.setDate, 1000));
-
-		setTimeout(app.setTransitions, 1000);
-	},
-
 	hours(time) {
 		const hourHand = document.querySelector(`.hourHand`);
-		const hours = time.getHours() <= 12
+		let hours = time.getHours() <= 12
 			? time.getHours()
-			: time.getHours - 12;
+			: time.getHours() - 12;
+		const minutes = time.getMinutes();
+		const seconds = time.getSeconds();
+
+		hours += (minutes / 60) + (seconds / 3600);
+
 		const hoursDegrees = ((hours / 12) * 360) + 90;
+
+		hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 		hoursDegrees === 90
 			? hourHand.style.transition = ``
-			: hourHand.style.transition = `all 3600s`;
-
-		hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+			: hourHand.style.transition = `all 1s linear`;
 	},
 
 	minutes(time) {
 		const minuteHand = document.querySelector(`.minuteHand`);
-		const minutes = time.getMinutes();
+		let minutes = time.getMinutes();
+		const seconds = time.getSeconds();
+
+		minutes += seconds / 60;
+
 		const minutesDegrees = ((minutes / 60) * 360) + 90;
+
+		minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
 		minutesDegrees === 90
 			? minuteHand.style.transition = ``
-			: minuteHand.style.transition = `all 60s`;
+			: minuteHand.style.transition = `all 1s linear`;
+	},
 
-		minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	onloadFunction() {
+		setInterval(app.setDate, 1000);
 	},
 
 	seconds(time) {
@@ -41,11 +43,11 @@ const app = {
 		const seconds = time.getSeconds();
 		const secondsDegrees = ((seconds / 60) * 360) + 90;
 
+		secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
 		secondsDegrees === 90
 			? secondHand.style.transition = ``
-			: secondHand.style.transition = `all .1s`;
-
-		secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+			: secondHand.style.transition = `all .1s cubic-bezier(0.26, 2.06, 1, 1)`;
 	},
 
 	setDate() {
@@ -55,14 +57,6 @@ const app = {
 		app.minutes(now);
 		app.hours(now);
 	},
-
-	// setTransitions() {
-	// 	const minuteHand = document.querySelector(`.minuteHand`);
-	// 	const hourHand = document.querySelector(`.hourHand`);
-
-	// 	minuteHand.style.transition = `all 60s`;
-	// 	hourHand.style.transition = `all 3600s`;
-	// },
 };
 
 window.onload = app.onloadFunction;
